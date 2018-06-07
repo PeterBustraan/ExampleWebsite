3f5cfe6ca3aedb3ae611bdd69018d3b70e144657
Peter Bustraan <peter795@mac.com>
2018-06-06 21:37:35 -0400

    Quick Pre Dinner Commit

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,7 +1,14 @@
 const express   = require('express');
 const exp       = express();
 
-
+//Setup simple listener for the main server
 exp.listen(8671, () => console.log('Simple Server Started Listening on 8671'))
 
-exp.get('/', (req, res) => res.send('Let\'s Get It Started'))
+//Static file Locations
+exp.use(express.static(__dirname + '/PageData'))
+exp.use(express.static(__dirname + '/css'))
+exp.use(express.static(__dirname + '/script'))
+
+//Simple Page Loader
+exp.get('/style.css', (req, res)  => res.sendFile('style.css'))
+exp.get('/', (req, res) => res.sendFile('index.html'))
