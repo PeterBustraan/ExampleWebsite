d54fe0c789bcfd92a7045d3b0dc0e0ed5b3c0d01
Peter Bustraan <peter795@mac.com>
2018-06-07 01:40:21 -0400

    Switched to pug before bed

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,15 +1,23 @@
 const express   = require('express');
-const exp       = express();
+      pug       = require('pug');
+      exp       = express();
 
 //Setup simple listener for the main server
-exp.listen(8671, () => console.log('Simple Server Started Listening on 8671'))
+    exp.listen(8671, () => console.log('Simple Server Started Listening on 8671'))
 
-//Static file Locations
-exp.use(express.static(__dirname + '/PageData'))
-exp.use(express.static(__dirname + '/css'))
-exp.use(express.static(__dirname + '/script'))
+//Jade Setup
+    exp.set('views','./PageData')
+    exp.set('view engine', 'pug')
+    exp.engine('pug', pug.__express)
+    exp.locals = {
+        title: 'Peter\'s Website Jam'
+    }
+
+    //Static files
+    exp.use(express.static(__dirname + '/css'))
+    exp.use(express.static(__dirname + '/script'))
 
 //Simple Page Loader
-exp.get('/style.css', (req, res)  => res.sendFile('style.css'))
-exp.get('/main.js', (req,res) => res.sendFile('main.js'))
-exp.get('/', (req, res) => res.sendFile('index.html'))
+    exp.get('/style.css', (req, res)  => res.sendFile('style.css'))
+    exp.get('/main.js', (req,res) => res.sendFile('main.js'))
+    exp.get('/', (req, res) => res.render('index'))
