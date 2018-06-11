890662d2e00aa2f19717a2218c0f80d6a20e185b
Peter Bustraan <peter795@mac.com>
2018-06-10 22:28:14 -0400

    Quick Ajax Cleaning

--- script/main.js
+++ script/main.js
@@ -6,8 +6,12 @@ $(document).ready(()=> {
             event.preventDefault();
         //Simple Ajax Page Loader
             let $this = $(event.target),
-                page  = $this.attr('href')
-            console.log(page)
+                page  = $this.attr('href'),
+                title = $this.text()
+        //Simple URL Fixer
+            if (page == '/index') page = '/';
             target.load(page + ' #dynamicLoad>*')
+        //Make For Usefull URLS
+            window.history.pushState('', title , page);
     });
 })
