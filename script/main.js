f235b5628ec5dd69f106ddc9395081d3f78ee091
Peter Bustraan <peter795@mac.com>
2018-06-10 22:17:52 -0400

    added jquery page loader before returning to css

--- script/main.js
+++ script/main.js
@@ -1,1 +1,13 @@
-alert("loaded");
+$(document).ready(()=> {
+    let target = $('#dynamicLoad')
+
+    $("a").click( (event) => {
+        //Prevent Links From going places
+            event.preventDefault();
+        //Simple Ajax Page Loader
+            let $this = $(event.target),
+                page  = $this.attr('href')
+            console.log(page)
+            target.load(page + ' #dynamicLoad>*')
+    });
+})
